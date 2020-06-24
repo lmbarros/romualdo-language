5097bbe51e8d00b0092dc81f5f2ddb0c88d7dd9b
Leandro Motta Barros <lmb@stackedboxes.org>
2020-06-24 19:29:30 -0300

    Add some untested parsing infrastructure

--- pkg/compiler/compiler.go
+++ pkg/compiler/compiler.go
@@ -8,45 +8,47 @@
 package compiler
 
 import (
-	"fmt"
-
 	"gitlab.com/stackedboxes/romulang/pkg/bytecode"
 	"gitlab.com/stackedboxes/romulang/pkg/scanner"
 	"gitlab.com/stackedboxes/romulang/pkg/token"
 )
 
+// parser holds some parsing-related data. I'd say it's not really a parser.
+type parser struct {
+	current   *token.Token // The current token.
+	previous  *token.Token // The previous token.
+	hadError  bool         // Did we find at least one error?
+	panicMode bool         // Are we in panic mode? (Parsing panic, nothing to do with Go panic!)
+}
+
 // Compiler is a Romualdo compiler.
 type Compiler struct {
+	p     *parser
+	s     *scanner.Scanner
+	chunk *bytecode.Chunk // The chunk the compiler is generating.
 }
 
 // New returns a new Compiler.
 func New() *Compiler {
-	return &Compiler{}
+	return &Compiler{
+		p:     &parser{},
+		chunk: &bytecode.Chunk{},
+	}
 }
 
-// Compile compiles source.
+// Compile compiles source and returns the chunk with the compiled bytecode. In
+// case of errors, returns nil.
 func (c *Compiler) Compile(source string) *bytecode.Chunk {
-	s := scanner.New(source)
+	// TODO: Candidate to change: I'd like to instantiate the scanner on New().
+	c.s = scanner.New(source)
 
-	line := -1
+	c.advance()
+	c.expression()
+	c.consume(token.KindEOF, "Expect end of expression.")
 
-	for {
-		tok := s.Token()
-
-		if tok.Line != line {
-			fmt.Printf("%4d ", tok.Line)
-			line = tok.Line
-		} else {
-			fmt.Print("   | ")
-		}
-
-		fmt.Printf("%18v '%v'\n", tok.Kind, tok.Lexeme)
-
-		if tok.Kind == token.KindEOF {
-			break
-		}
+	if c.p.hadError {
+		return nil
+	} else {
+		return c.chunk
 	}
-
-	// TODO: Dummy return
-	return &bytecode.Chunk{}
 }
---
+++ pkg/compiler/front_end.go
@@ -0,0 +1,72 @@
+/******************************************************************************\
+* The Romualdo Language                                                        *
+*                                                                              *
+* Copyright 2020 Leandro Motta Barros                                          *
+* Licensed under the MIT license (see LICENSE.txt for details)                 *
+\******************************************************************************/
+
+package compiler
+
+import (
+	"fmt"
+	"os"
+
+	"gitlab.com/stackedboxes/romulang/pkg/token"
+)
+
+// advance advances the parser by one token. This will report errors for each
+// error token found; callers will only see the non-error tokens.
+func (c *Compiler) advance() {
+	c.p.previous = c.p.current
+
+	for {
+		c.p.current = c.s.Token()
+		if c.p.current.Kind != token.KindError {
+			break
+		}
+
+		c.errorAtCurrent(c.p.current.Lexeme)
+	}
+}
+
+// consume consumes the current token (and advances the parser), assuming it is
+// of a given kind. If it is not of this kind, reports this is an error with a
+// given error message.
+func (c *Compiler) consume(kind token.Kind, message string) {
+	if c.p.current.Kind == kind {
+		c.advance()
+		return
+	}
+
+	c.errorAtCurrent(message)
+}
+
+// errorAtCurrent reports an error at the current (c.p.current) token.
+func (c *Compiler) errorAtCurrent(message string) {
+	c.errorAt(c.p.current, message)
+}
+
+// error reports an error at the token we just consumed (c.p.previous).
+func (c *Compiler) error(message string) {
+	c.errorAt(c.p.previous, message)
+}
+
+func (c *Compiler) errorAt(tok *token.Token, message string) {
+	if c.p.panicMode {
+		return
+	}
+
+	c.p.panicMode = true
+
+	fmt.Fprintf(os.Stderr, "[line %v] Error", tok.Line)
+
+	if tok.Kind == token.KindEOF {
+		fmt.Fprintf(os.Stderr, " at end", tok.Line)
+	} else if tok.Kind == token.KindError {
+		// Nothing.
+	} else {
+		fmt.Fprintf(os.Stderr, " at '%v'", tok.Lexeme)
+	}
+	fmt.Fprintf(os.Stderr, ": %v\n", message)
+	c.p.hadError = true
+}
